7864d1283953415bbdd1cfc13cc0c6bae538d1e0
matteo895 <matteoventuri1198@gmail.com>
2024-02-07 17:39:37 +0100

    fixed-question

--- js/quiz.js
+++ js/quiz.js
@@ -297,7 +297,9 @@ const questions = {
     },
   ],
 };
+
 let currentQuestionIndex = 0;
+let correctAnswers = []; // Array per memorizzare le risposte corrette
 
 // Funzione per visualizzare la domanda corrente e le risposte
 function displayQuestion() {
@@ -313,6 +315,10 @@ function displayQuestion() {
   // Estrae la domanda corrente dall'array results
   const currentQuestion = questions.results[currentQuestionIndex];
 
+  //numero domanda corrente
+  //const questionNum = currentQuestionIndex + 1;
+  const questionNum = document.querySelector("#questionNum");
+  questionNum.innerHTML = `QUESTION ` + (currentQuestionIndex + 1);
   // Aggiunge la domanda corrente
   const questionText = document.createElement("strong");
   questionText.textContent = currentQuestion.question;
@@ -339,6 +345,7 @@ function displayQuestion() {
 function checkAnswer(isCorrect) {
   if (isCorrect) {
     alert("Correct answer!");
+    correctAnswers.push(questions.results[currentQuestionIndex].correct_answer); // Aggiunge la risposta corretta all'array
   } else {
     alert(
       "Incorrect answer. The correct answer is: " +
@@ -352,8 +359,45 @@ function checkAnswer(isCorrect) {
     displayQuestion();
   } else {
     alert("End of questions!");
+    console.log("Correct answers:", correctAnswers); // Stampa tutte le risposte corrette alla fine del quiz
+    return correctAnswers.length;
   }
 }
 
 // Avvia il gioco mostrando la prima domanda
 displayQuestion();
+
+// Avvia il gioco mostrando la prima domanda
+
+//TIMER
+
+displayQuestion();
+
+function startTimer() {
+  const countdownElement = document.getElementById("n-16");
+  let countdown = 0; // Inizia da 0
+  const maxCountdown = 60;
+
+  const circle = document.querySelector(".circle"); // Seleziona il cerchio timer
+
+  const timerInterval = 1000; // Intervallo di aggiornamento del timer in millisecondi
+
+  let progress = 0; // Inizializza la variabile di progresso
+
+  let timer = setInterval(() => {
+    countdown++;
+    if (countdown > maxCountdown) {
+      // Se il countdown supera il massimo, ripartiamo da 0
+      countdown = 0;
+    }
+    countdownElement.textContent = countdown; // Aggiorna il valore del countdown
+
+    // Calcola il progresso della barra di caricamento
+    progress = (countdown / maxCountdown) * 100;
+
+    // Imposta la larghezza della barra di caricamento
+    circle.style.background = `conic-gradient(#00ffff ${progress}%, rgba(255, 255, 255, 0.3) ${progress}%) border-box`;
+  }, timerInterval);
+}
+
+startTimer();
