ae2335c7f5a62cf2e9df00a366078ab31b9531b8
matteo895 <matteoventuri1198@gmail.com>
2024-02-07 11:42:27 +0100

    array-quiz

---
+++ js/quiz.js
@@ -0,0 +1,103 @@
+const array = {
+  response_code: 0,
+  results: [
+    {
+      type: "multiple",
+      difficulty: "easy",
+      category: "Science: Computers",
+      question:
+        "On Twitter, what was the original character limit for a Tweet?",
+      correct_answer: "140",
+      incorrect_answers: ["120", "160", "100"],
+    },
+    {
+      type: "multiple",
+      difficulty: "easy",
+      category: "Science: Computers",
+      question:
+        "Which programming language shares its name with an island in Indonesia?",
+      correct_answer: "Java",
+      incorrect_answers: ["Python", "C", "Jakarta"],
+    },
+    {
+      type: "multiple",
+      difficulty: "easy",
+      category: "Science: Computers",
+      question: "What does the Prt Sc button do?",
+      correct_answer:
+        "Captures what&#039;s on the screen and copies it to your clipboard",
+      incorrect_answers: [
+        "Nothing",
+        "Saves a .png file of what&#039;s on the screen in your screenshots folder in photos",
+        "Closes all windows",
+      ],
+    },
+    {
+      type: "multiple",
+      difficulty: "easy",
+      category: "Science: Computers",
+      question: "In computing, what does MIDI stand for?",
+      correct_answer: "Musical Instrument Digital Interface",
+      incorrect_answers: [
+        "Musical Interface of Digital Instruments",
+        "Modular Interface of Digital Instruments",
+        "Musical Instrument Data Interface",
+      ],
+    },
+    {
+      type: "boolean",
+      difficulty: "easy",
+      category: "Science: Computers",
+      question:
+        "The programming language &quot;Python&quot; is based off a modified version of &quot;JavaScript&quot;.",
+      correct_answer: "False",
+      incorrect_answers: ["True"],
+    },
+    {
+      type: "multiple",
+      difficulty: "easy",
+      category: "Science: Computers",
+      question:
+        "When Gmail first launched, how much storage did it provide for your email?",
+      correct_answer: "1GB",
+      incorrect_answers: ["512MB", "5GB", "Unlimited"],
+    },
+    {
+      type: "multiple",
+      difficulty: "easy",
+      category: "Science: Computers",
+      question: "How many kilobytes in one gigabyte (in decimal)?",
+      correct_answer: "1000000",
+      incorrect_answers: ["1024", "1000", "1048576"],
+    },
+    {
+      type: "multiple",
+      difficulty: "easy",
+      category: "Science: Computers",
+      question: "What does the computer software acronym JVM stand for?",
+      correct_answer: "Java Virtual Machine",
+      incorrect_answers: [
+        "Java Vendor Machine",
+        "Java Visual Machine",
+        "Just Virtual Machine",
+      ],
+    },
+    {
+      type: "multiple",
+      difficulty: "easy",
+      category: "Science: Computers",
+      question:
+        "The numbering system with a radix of 16 is more commonly referred to as ",
+      correct_answer: "Hexidecimal",
+      incorrect_answers: ["Binary", "Duodecimal", "Octal"],
+    },
+    {
+      type: "boolean",
+      difficulty: "easy",
+      category: "Science: Computers",
+      question: "The Windows 7 operating system has six main editions.",
+      correct_answer: "True",
+      incorrect_answers: ["False"],
+    },
+  ],
+};
